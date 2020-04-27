5ab0cc6acb3f36dcfb191fc58ac7a42248dd71d1
Gaurav Sharma <kvdgaurav@gmail.com>
2020-04-27 14:49:10 -0700

    Updates

---
+++ precache-manifest.1870656086fee2806f36ab2f0974520e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e1ace4df39a8a857521e9112f3137a6",
+    "url": "/To-do-app/index.html"
+  },
+  {
+    "revision": "da3996819d8ef12f8ebf",
+    "url": "/To-do-app/static/css/main.184154b0.chunk.css"
+  },
+  {
+    "revision": "5e65f356aba702b90f54",
+    "url": "/To-do-app/static/js/2.10851444.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-app/static/js/2.10851444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da3996819d8ef12f8ebf",
+    "url": "/To-do-app/static/js/main.ba08af91.chunk.js"
+  },
+  {
+    "revision": "d2ad16b0c14a401315d6",
+    "url": "/To-do-app/static/js/runtime-main.1d6b5aa5.js"
+  }
+]);
